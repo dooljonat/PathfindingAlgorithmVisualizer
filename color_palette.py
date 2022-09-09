01fcd1b5e871ff2587b6722b0e4367cc9b079fa9
Jonathan Dooley <jonathandool75@gmail.com>
2022-09-08 20:12:38 -0400

    Made code more readable

--- color_palette.py
+++ color_palette.py
@@ -1,5 +1,3 @@
-# TODO: I like these colors for now, I might change them in the future
-#BACKGROUND_COLOR = (252, 248, 232) # Change from beige to pale gray, cooler tones
 BACKGROUND_COLOR = (229, 229, 229)
 GRIDLINE_COLOR = (211, 211, 211)
 START_POINT_COLOR = (89, 206, 143)
--- main.py
+++ main.py
@@ -4,9 +4,8 @@
 # Requirements:
 #       -- Pygame
 # CMD LINE ARGUMENTS:
-#       -- 1. rows
-#       -- 2. columns
-#   e.g. py main.py 20 20
+#       -- 1. grid size
+#   e.g. py main.py 20
 
 import sys
 
@@ -14,49 +13,40 @@ import pygame
 pygame.init()
 
 import color_palette as colors
+import draw as draw
 
 
 # Initialize grid size from cmd line arguments
-# TODO: CHECK IF CMD LINE ARGS EXIST FIRST, SET DEFAULT AS 20
 if len(sys.argv) > 1:
     GRID_SIZE = int(sys.argv[1])
 else:
     GRID_SIZE = 20
+
 # Initialize board
 # TODO: Make this editable by the user, e.g. rows and columns
 #       (not predefined)
 board = [0] * GRID_SIZE
 for i in range(GRID_SIZE):
     board[i] = [0] * GRID_SIZE
+
+# Set start point
+board[0][GRID_SIZE-1] = 2
+# Set end point
+board[GRID_SIZE-1][0] = 3
 # 0 = EMPTY SPACE
 # 1 = WALL
 # 2 = START POINT
 # 3 = END POINT
 
-# board = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,],
-#     [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-#     [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-# ]
 
 def main():
+    """
+    Runs the pygame clock, event handler, and main loop
+    Args:
+        N/A
+    Returns:
+        VOID
+    """
     # Initialize global SCREEN and clock variables
     global SCREEN, CLOCK
 
@@ -85,40 +75,10 @@ def main():
         SCREEN.fill(colors.BACKGROUND_COLOR)
 
         # Draw board to screen
-        for y in range(len(board)):
-            for x in range(len(board[y])):
-                # If board coordinate is a...
-                # WALL
-                if board[x][y] == 1:
-                    coord_x = x*cell_width
-                    coord_y = y*cell_height
-                    pygame.draw.rect(SCREEN, colors.WALL_COLOR, 
-                        pygame.Rect(coord_x, coord_y, cell_width, cell_height))
-                # STARTPOINT
-                elif board[x][y] == 2:
-                    coord_x = x*cell_width
-                    coord_y = y*cell_height
-                    pygame.draw.rect(SCREEN, colors.START_POINT_COLOR, 
-                        pygame.Rect(coord_x, coord_y, cell_width, cell_height))
-                # ENDPOINT
-                elif board[x][y] == 3:
-                    coord_x = x*cell_width
-                    coord_y = y*cell_height
-                    pygame.draw.rect(SCREEN, colors.END_POINT_COLOR, 
-                        pygame.Rect(coord_x, coord_y, cell_width, cell_height))
+        draw.draw_board(pygame, SCREEN, board, cell_width, cell_height)
 
         # Draw grid overlay to screen
-        for i in range(GRID_SIZE):
-            # Draw column
-            column_start_x = (cell_width*i) + cell_width
-            pygame.draw.line(SCREEN, colors.GRIDLINE_COLOR, 
-                (column_start_x, 0), (column_start_x, HEIGHT))
-            
-            # Draw row
-            row_start_y = (cell_height*i) + cell_height
-            pygame.draw.line(SCREEN, colors.GRIDLINE_COLOR, 
-                (0, row_start_y), (WIDTH, row_start_y))
-
+        draw.draw_grid_overlay(pygame, SCREEN, board, cell_width, cell_height)
 
         # Flip display
         pygame.display.flip()
